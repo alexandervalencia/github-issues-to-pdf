8d6375dc3a7c6c115f93277bfe405294d62d622a
Alexander Valencia <alexander.valencia@liferay.com>
2017-04-11 18:16:33 -0700

    adds limited functionality of github

--- gip.js
+++ gip.js
@@ -1,35 +1,117 @@
-"use strict";
-var page = require('webpage').create(),
-	config = require('./gip-config.js'),
-	url = config.url(),
-	output = config.output();
+var accountType;
+var allOrUniqueRepos;
+var customHeaders = {'user-agent': 'GitHub-Issues-to-PDF'};
+var fs = require('fs');
+var GitHubApi = require('github');
+var job = {};
+var orgName;
+var repoName;
+var rls = require('readline-sync');
 
-page.viewportSize = { width: 600, height: 600 };
 
-page.paperSize = {
-	format: 'letter',
-	orientation: 'portrait',
-	margin: '1cm'
-};
-
-page.open(url,
-	function (status) {
-		if (status !== 'success') {
-			console.log('Unable to load the URL!');
-
-			phantom.exit(1);
-
-		} else {
-			window.setTimeout(
-				function () {
-					page.render(output);
-					console.log('Render complete!');
-
-					phantom.exit();
-
-				},
-				200
-			);
-		}
+var github = new GitHubApi(
+	{
+		debug: true,
+		followRedirects: false,
+		headers: customHeaders,
+		host: 'api.github.com',
+		pathPrefix: '',
+		timeout: 10000
 	}
 );
+
+function getAccountType() {
+	return rls.keyInSelect(accountTypes, 'What is the account type you\'re searching for? ');
+}
+
+function getAllOrUniqueRepos(repoOptions) {
+	return rls.keyInSelect(repoOptions, 'Do you want issues from all the repositories or a specific one? ');
+}
+
+function getAllOrgRepos(pageCount) {
+	if (!pageCount) {
+		pageCount = 1;
+	}
+
+	github.repos.getForOrg({
+		org: orgName,
+		page: pageCount,
+		per_page: 100,
+		type: 'public'
+	}, function(err, res) {
+		fs.writeFileSync((generateFileName() + '_page' + pageCount + '.json'), JSON.stringify(res), null);
+
+		if (github.hasNextPage(res)) {
+			getAllOrgRepos(pageCount + 1);
+		}
+	});
+}
+
+function getIssues(repoName, pageCount) {
+	if (!pageCount) {
+		pageCount = 1;
+	}
+
+	github.issues.getForRepo({
+		owner: orgName,
+		page: pageCount,
+		per_page: 100,
+		repo: repoName,
+		since: '2016-01-01T00:00:01Z',
+		state: 'all'
+	}, function(err, res) {
+		fs.writeFileSync((orgName + '_' + repoName + '_' + 'issues_page' + pageCount + '.json'), JSON.stringify(res), null);
+
+		if (github.hasNextPage(res)) {
+			getIssues(repoName, pageCount + 1);
+		}
+
+	});
+}
+
+function generateFileName() {
+	if (repoName != null) {
+		return orgName + '_' + repoName;
+	}
+	return orgName;
+}
+
+function handleOrgRepoList() {
+	var pulledRepoList = JSON.parse(getAllOrgRepos());
+	var pulledIssuesList = JSON.parse(getAllIssues());
+	var repoList;
+	var masterList;
+
+	for (i = 0; i < pulledRepoList.data.length; i++) {
+		newRepoList.organization.repo.push({
+			name: pulledRepoList.data[i].name
+		});
+	}
+
+	return masterList;
+}
+
+/**
+github.authenticate({
+	token: '',
+	type: 'token'
+});
+*/
+
+accountType = getAccountType(['organization', 'user']);
+
+orgName = rls.question('Please enter the name of the ' + accountType + ' account you would like to query (required): ');
+
+allOrUniqueRepos = getAllOrUniqueRepos(['all', 'a specific repo']);
+
+if (allOrUniqueRepos === 1) {
+	repoName = rls.question('Please enter the name of the specific repository you want: ');
+}
+
+getAllOrgRepos();
+if (allOrUniqueRepos === 0) {
+	getIssues(all);
+}
+else {
+	getIssues(repoName);
+}
